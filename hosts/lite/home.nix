bd3f6c4b46360f438bdc12c75cff827ba3e44dfd
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-02-16 11:32:14 -0800

    fixing issues

--- hosts/lite/home.nix
+++ hosts/lite/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, system, inputs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -27,7 +27,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -44,12 +44,13 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    waybar
+    mako
+    libnotify
+    hyprpaper
+    rofi-wayland
+
     inputs.zen-browser.packages."${system}".default
-    pkgs.waybar
-    pkgs.mako
-    pkgs.libnotify
-    pkgs.hyprpaper
-    pkgs.rofi-wayland
   ];
 
   programs.git = {
