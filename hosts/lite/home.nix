a1a0ae0dd2165f70937b48ba5bdf8a88ca9ce814
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-02-16 23:41:29 -0800

    added hyprland option

--- hosts/lite/home.nix
+++ hosts/lite/home.nix
@@ -5,6 +5,8 @@
     ./../../modules/user/default.nix
   ];
 
+  hyprland.enable = true;
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "zach";
--- modules/user/hyprland.nix
+++ modules/user/hyprland.nix
@@ -1,4 +1,7 @@
 { pkgs, lib, config, ... }: {
+  options = {
+    hyprland.enable = lib.mkEnableOption "enables hyprland module";
+  };
   config = lib.mkIf config.hyprland.enable {
     wayland.windowManager.hyprland = {
       enable = true;
