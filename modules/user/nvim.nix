306fa7d94c8ac8d5f1e06cb657084f90ffc72eee
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-02-17 23:59:14 -0800

    put parser back in

--- modules/user/nvim.nix
+++ modules/user/nvim.nix
@@ -115,18 +115,18 @@
       };
     };
 
-    # # https://github.com/nvim-treesitter/nvim-treesitter#i-get-query-error-invalid-node-type-at-position
-    # xdg.configFile."nvim/parser".source =
-    #   let
-    #     parsers = pkgs.symlinkJoin {
-    #       name = "treesitter-parsers";
-    #       paths = (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: with plugins; [
-    #         c
-    #         lua
-    #       ])).dependencies;
-    #     };
-    #   in
-    #   "${parsers}/parser";
+    # https://github.com/nvim-treesitter/nvim-treesitter#i-get-query-error-invalid-node-type-at-position
+    xdg.configFile."nvim/parser".source =
+      let
+        parsers = pkgs.symlinkJoin {
+          name = "treesitter-parsers";
+          paths = (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: with plugins; [
+            c
+            lua
+          ])).dependencies;
+        };
+      in
+      "${parsers}/parser";
 
     # # Normal LazyVim config here, see https://github.com/LazyVim/starter/tree/main/lua
     # xdg.configFile."nvim/lua".source = ./lua;
