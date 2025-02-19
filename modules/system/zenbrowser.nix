63031238f5d06d01998a59cb1fc60666c0e9bc3e
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-02-19 12:55:43 -0800

    fixed typo

--- modules/system/zenbrowser.nix
+++ modules/system/zenbrowser.nix
@@ -3,7 +3,7 @@
     zenbrowser.enable = lib.mkEnableOption "enables zenbrowser module";
   };
   config = lib.mkIf config.zenbrowser.enable {
-    inputs.zen-browser.packages."${system}".default # beta
+    inputs.zen-browser.packages."${system}".default;
   };
 }
 
