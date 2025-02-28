7e540b79b714349f892453a009695c589805c13a
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-02-27 23:56:04 -0800

    more refactoring

--- hosts/nixos-lite/configuration.nix
+++ hosts/nixos-lite/configuration.nix
@@ -1,42 +1,35 @@
-{ pkgs, lib, inputs, config, system, ... }:
-{
-  options = {
-    userName = lib.mkOption { type = lib.types.str; };
-    userFullName = lib.mkOption { type = lib.types.str; };
-    hostName = lib.mkOption { type = lib.types.str; };
-  };
-  config = rec {
-    userName = "zach";
-    userFullName = "Zach Putman";
-    hostName = "nixos-lite";
+{ pkgs, lib, inputs, config, ... }:
+rec {
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./../../modules/system
+      inputs.home-manager.nixosModules.default
+      inputs.catppuccin.nixosModules.catppuccin
+    ];
 
-    imports =
-      [
-        ./hardware-configuration.nix
-        ./../../modules/system
-        inputs.home-manager.nixosModules.default
-        inputs.catppuccin.nixosModules.catppuccin
+  userName = "zach";
+  userFullName = "Zach Putman";
+  hostName = "nixos-lite";
+  
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = hostName;
+
+  zsh.enable = true;
+  hyprland.enable = true;
+  twingate.enable = true;
+
+  home-manager = {
+    users."${userName}" = {
+      imports = [
+        ./home.nix
+        inputs.catppuccin.homeManagerModules.catppuccin
       ];
-    
-    # Bootloader.
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-
-    networking.hostName = hostName;
-
-    zsh.enable = true;
-    hyprland.enable = true;
-    twingate.enable = true;
-
-    home-manager = {
-      users."${userName}" = {
-        imports = [
-          ./home.nix
-          inputs.catppuccin.homeManagerModules.catppuccin
-        ];
-      };
     };
-
-    system.stateVersion = "24.11";
   };
+
+  system.stateVersion = "24.11";
 }
--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,31 +1,38 @@
 { pkgs, lib, config, ... }: {
-  imports = [
-    ./locale.nix
-    ./settings.nix
-    ./user.nix
-    ./zsh.nix
-    ./wifi.nix
-    ./printing.nix
-    ./sound.nix
-    ./xserver.nix
-    ./hyprland.nix
-    ./touchpad.nix
-    ./nvim.nix
-    ./git.nix
-    ./ssh.nix
-    ./suid.nix
-    ./zenbrowser.nix
-    ./twingate.nix
-  ];
+  options = {
+    userName = lib.mkOption { type = lib.types.str; };
+    userFullName = lib.mkOption { type = lib.types.str; };
+    hostName = lib.mkOption { type = lib.types.str; };
+  };
+  config = {
+    imports = [
+      ./locale.nix
+      ./settings.nix
+      ./user.nix
+      ./zsh.nix
+      ./wifi.nix
+      ./printing.nix
+      ./sound.nix
+      ./xserver.nix
+      ./hyprland.nix
+      ./touchpad.nix
+      ./nvim.nix
+      ./git.nix
+      ./ssh.nix
+      ./suid.nix
+      ./zenbrowser.nix
+      ./twingate.nix
+    ];
 
-  user.enable = lib.mkDefault true;
-  wifi.enable = lib.mkDefault true;
-  printing.enable = lib.mkDefault true;
-  zzsound.enable = lib.mkDefault true;
-  xserver.enable = lib.mkDefault true;
-  programs.firefox.enable = lib.mkDefault true;
-  zenbrowser.enable = lib.mkDefault true;
-  nvim.enable = lib.mkDefault true;
-  programs.git.enable = lib.mkDefault true;
+    user.enable = lib.mkDefault true;
+    wifi.enable = lib.mkDefault true;
+    printing.enable = lib.mkDefault true;
+    zzsound.enable = lib.mkDefault true;
+    xserver.enable = lib.mkDefault true;
+    programs.firefox.enable = lib.mkDefault true;
+    zenbrowser.enable = lib.mkDefault true;
+    nvim.enable = lib.mkDefault true;
+    programs.git.enable = lib.mkDefault true;
+  };
 }
 
