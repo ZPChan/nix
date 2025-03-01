ea20b1bd93d849891a62320805ac1a20b97b5a89
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-03-01 07:16:11 -0800

    turned on autosuggest

--- modules/user/zsh.nix
+++ modules/user/zsh.nix
@@ -1,6 +1,10 @@
 { pkgs, lib, config, ... }: {
   config = lib.mkIf config.programs.zsh.enable {
     programs.starship.enable = lib.mkDefault true;
+    programs.zsh = {
+      autosuggestion.enable = true;
+    };
+    environment.pathsToLink = [ "/share/zsh"];
   };
 }
 
