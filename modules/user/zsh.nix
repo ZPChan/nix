6919c277cd6fd1ab7367e97cbe212325858a1868
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-03-01 07:37:14 -0800

    trying some zsh plugins

--- modules/user/zsh.nix
+++ modules/user/zsh.nix
@@ -3,6 +3,14 @@
     programs.starship.enable = lib.mkDefault true;
     programs.zsh = {
       autosuggestion.enable = true;
+      antidote = {
+        enable = true;
+        plugins = [
+          "zsh-users/zsh-syntax-highlighting"
+          "zsh-users/zsh-autosuggestions"
+          "zsh-users/zsh-history-substring-search"
+        ];
+      };
     };
   };
 }
