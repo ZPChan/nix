65caaddc3f17e30f3d77d723c9539c5dcaef7994
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-03-02 16:35:22 -0800

    added lazyvim mason modules

--- modules/user/nvim.nix
+++ modules/user/nvim.nix
@@ -2,19 +2,40 @@
   config = lib.mkIf config.programs.neovim.enable {
     programs.neovim = {
       
+      defaultEditor = true;
       viAlias = true;
       vimAlias = true;
       vimdiffAlias = true;
 
       extraPackages = with pkgs; [
-        lua-language-server
-        stylua
         ripgrep
         gcc
         fzf
         fd
         nil
+
+        bash-debug-adapter
+        bash-language-server
+        csharpier
+        djlint
+        docker-compose-language-service
+        dockerfile-language-server
+        hadolint
+        helm-ls
+        json-lsp
+        lua-language-server
+        markdown-toc
         markdownlint-cli2
+        marksman
+        netcoredbg
+        omnisharp
+        semgrep
+        shellcheck
+        shfmt
+        sqlfluff
+        stylua
+        vtsls
+        yaml-language-server
       ];
 
       plugins = with pkgs.vimPlugins; [
