b0be24e4a66807aa10331529ea71cb97b5861711
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-03-25 00:24:33 -0700

    trying new tmux style

--- modules/user/tmux.nix
+++ modules/user/tmux.nix
@@ -18,19 +18,39 @@
       customPaneNavigationAndResize = true;
       extraConfig = ''
         set-option -g status-position top
-        set-option -g allow-rename off
 
-        # Configure the catppuccin plugin
         set -g @catppuccin_flavor "mocha"
-        set -g @catppuccin_window_status_style "rounded"
 
-        # Make the status line pretty and add some modules
         set -g status-right-length 100
-        set -g status-left-length 100
         set -g status-left ""
-        set -g status-right "#{E:@catppuccin_status_application}"
-        set -ag status-right "#{E:@catppuccin_status_session}"
 
+        # Window
+        set -g @catppuccin_window_status_style "custom"
+        set -g window-status-separator ""
+
+        ## Window global/default configuration
+        set -g @catppuccin_window_default_text " #{window_name}"
+        set -g @catppuccin_window_status "icon"
+        set -g @catppuccin_window_default_fill "number"
+        set -g @catppuccin_window_number_position "left"
+
+        set -g @catppuccin_window_left_separator "█"
+        set -g @catppuccin_window_middle_separator "█"
+        set -g @catppuccin_window_right_separator "█"
+
+        ## Window current configuration
+        set -g @catppuccin_window_current_text "#{window_name}"
+        set -g @catppuccin_window_current_fill "all"
+        set -g @catppuccin_window_current_middle_separator "#[reverse] 󰿟 #[noreverse]"
+
+        # Status modules config
+        set -g @catppuccin_date_time_text "%d-%m %H:%M"
+
+        # Status
+        set -gF  status-right "#{@catppuccin_status_directory}"
+        set -agF status-right "#{@catppuccin_status_session}"
+        set -agF status-right "#{@catppuccin_status_user}"
+        set -agF status-right "#{@catppuccin_status_host}"
       '';
       plugins = with pkgs; [
         tmuxPlugins.catppuccin
