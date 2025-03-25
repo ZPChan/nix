db84d5400502e68e21a8a65677edd3a14c93561c
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-03-25 00:48:38 -0700

    trying plugin extraConfig

--- modules/user/tmux.nix
+++ modules/user/tmux.nix
@@ -19,34 +19,11 @@
       extraConfig = ''
         set-option -g status-position top
 
-        set -g @catppuccin_flavor "mocha"
-
         set -g status-right-length 100
-
-        # Window
-        set -g @catppuccin_window_status_style "custom"
+        set -g status-left ""
         set -g window-status-separator ""
 
-        ## Window global/default configuration
-        set -g @catppuccin_window_default_text " #{window_name}"
-        set -g @catppuccin_window_status "icon"
-        set -g @catppuccin_window_default_fill "number"
-        set -g @catppuccin_window_number_position "left"
-
-        set -g @catppuccin_window_left_separator "█"
-        set -g @catppuccin_window_middle_separator "█"
-        set -g @catppuccin_window_right_separator "█"
-
-        ## Window current configuration
-        set -g @catppuccin_window_current_text "#{window_name}"
-        set -g @catppuccin_window_current_fill "all"
-        set -g @catppuccin_window_current_middle_separator "#[reverse] 󰿟 #[noreverse]"
-
-        # Status modules config
-        set -g @catppuccin_date_time_text "%d-%m %H:%M"
-
         # Status
-        set -g status-left ""
         set -gF  status-right "#{@catppuccin_status_directory}"
         set -agF status-right "#{@catppuccin_status_session}"
         set -agF status-right "#{@catppuccin_status_user}"
@@ -55,7 +32,24 @@
       plugins = with pkgs; [
         {
           plugin = tmuxPlugins.catppuccin;
-          extraConfig = "# where am i?";
+          extraConfig = ''
+            set -g @catppuccin_flavor "mocha"
+            set -g @catppuccin_window_status_style "custom"
+
+            set -g @catppuccin_window_default_text " #{window_name}"
+            set -g @catppuccin_window_status "icon"
+            set -g @catppuccin_window_default_fill "number"
+            set -g @catppuccin_window_number_position "left"
+
+            set -g @catppuccin_window_left_separator "█"
+            set -g @catppuccin_window_middle_separator "█"
+            set -g @catppuccin_window_right_separator "█"
+
+            ## Window current configuration
+            set -g @catppuccin_window_current_text "#{window_name}"
+            set -g @catppuccin_window_current_fill "all"
+            set -g @catppuccin_window_current_middle_separator "#[reverse] 󰿟 #[noreverse]"
+          '';
         }
       ];
     };
