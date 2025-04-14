f223b3fbb17b65f5cdcba9d9bea812acf766ac2c
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-04-14 13:23:38 -0700

    pinned nixos-wsl to a specific release branch where copy works

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/2411.6.0";
     catppuccin.url = "github:catppuccin/nix";
     home-manager = {
       url = "github:nix-community/home-manager";
