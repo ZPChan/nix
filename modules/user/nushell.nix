daae574bfa8991814070fa21697deb69d4c24050
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-04-17 08:33:43 -0700

    set nushell to default vi mode

--- modules/user/nushell.nix
+++ modules/user/nushell.nix
@@ -13,6 +13,7 @@
       enable = true;
       extraConfig = ''
         $env.LS_COLORS = (vivid generate catppuccin-mocha)
+        $env.config.edit_mode = 'vi'
       '';
     };
     catppuccin.nushell.enable = true;
