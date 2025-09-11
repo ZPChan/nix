ff50f92033a667affe1bbf8843f1051b8d27bb20
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-09-11 13:48:42 -0700

    temporary fix for bug with catppucin and bufferline

--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -5,4 +5,13 @@ return {
 			colorscheme = "catppuccin",
 		},
 	},
+	-- Remove this once fix is out: https://github.com/LazyVim/LazyVim/pull/6354
+	{
+		"catppuccin",
+		optional = true,
+		opts = function()
+			local bufferline = require("catppuccin.groups.integrations.bufferline")
+			bufferline.get = bufferline.get or bufferline.get_theme
+		end,
+	},
 }
