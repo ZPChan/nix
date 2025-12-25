d32e49b3eb0d5b10bc6674266dcd260ea4c0b89a
zach <23585500+ZPChan@users.noreply.github.com>
2025-12-25 12:22:14 -0800

    updated adguard setup

--- modules/system/containers/adguardhome.nix
+++ modules/system/containers/adguardhome.nix
@@ -10,21 +10,25 @@
   };
   config = lib.mkIf config.my.docker.adguardhome.enable {
     my.docker.enable = lib.mkDefault true;
-    services.adguardhome = {
-      service.image = "adguard/adguardhome";
-      service.volumes = [
-        "/var/containers/adguardhome/work:/opt/adguardhome/work"
-        "/var/containers/adguardhome/conf:/opt/adguardhome/conf"
-      ];
-      service.ports = [
-        "53:53/tcp"
-        "53:53/udp"
-        "80:80/tcp"
-        "443:443/tcp"
-        "443:443/udp"
-        "3000:3000/tcp"
-        "853:853/tcp"
-      ];
+    virtualisation.arion = {
+      backend = "docker";
+      projects.adguardhome.settings.services.adguardhome.service = {
+        image = "adguard/adguardhome";
+        restart = "always";
+        volumes = [
+          "/var/containers/adguardhome/work:/opt/adguardhome/work"
+          "/var/containers/adguardhome/conf:/opt/adguardhome/conf"
+        ];
+        ports = [
+          "53:53/tcp"
+          "53:53/udp"
+          "80:80/tcp"
+          "443:443/tcp"
+          "443:443/udp"
+          "3000:3000/tcp"
+          "853:853/tcp"
+        ];
+      };
     };
   };
 }
