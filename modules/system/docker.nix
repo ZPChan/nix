86de5716f998068a03d43cf05bdfcc17929aea84
zach <23585500+ZPChan@users.noreply.github.com>
2025-12-25 11:25:59 -0800

    added arion

--- modules/system/docker.nix
+++ modules/system/docker.nix
@@ -9,6 +9,9 @@
     my.docker.enable = lib.mkEnableOption "enables docker module";
   };
   config = lib.mkIf config.my.docker.enable {
+    environment.systemPackages = [
+      pkgs.arion
+    ];
     virtualisation.docker.enable = true;
     users.users."${config.my.user.userName}" = {
       extraGroups = [
