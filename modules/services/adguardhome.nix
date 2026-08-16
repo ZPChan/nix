3ea9f7496197c45712e591496488d3f9f128aa58
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2026-08-16 10:03:08 -0700

    put arion import back in, oops!

--- modules/services/adguardhome.nix
+++ modules/services/adguardhome.nix
@@ -1,38 +1,35 @@
 { inputs, ... }:
 {
-  flake.modules.nixos.adguardhome =
-    {
-      modulesPath,
-      lib,
-      ...
-    }:
-    {
-      virtualisation.arion = {
-        projects.adguardhome = {
-          serviceName = "adguardhome";
-          settings = {
-            project.name = "adguardhome";
-            services = {
-              adguardhome.service = {
-                image = "adguard/adguardhome";
-                restart = "unless-stopped";
-                volumes = [
-                  "/var/containers/adguardhome/work:/opt/adguardhome/work"
-                  "/var/containers/adguardhome/conf:/opt/adguardhome/conf"
-                ];
-                ports = [
-                  "53:53/tcp"
-                  "53:53/udp"
-                  "80:80/tcp"
-                  "443:443/tcp"
-                  "443:443/udp"
-                  "3000:3000/tcp"
-                  "853:853/tcp"
-                ];
-              };
+  flake.modules.nixos.adguardhome = {
+    imports = with inputs.self.modules.nixos; [
+      arion
+    ];
+    virtualisation.arion = {
+      projects.adguardhome = {
+        serviceName = "adguardhome";
+        settings = {
+          project.name = "adguardhome";
+          services = {
+            adguardhome.service = {
+              image = "adguard/adguardhome";
+              restart = "unless-stopped";
+              volumes = [
+                "/var/containers/adguardhome/work:/opt/adguardhome/work"
+                "/var/containers/adguardhome/conf:/opt/adguardhome/conf"
+              ];
+              ports = [
+                "53:53/tcp"
+                "53:53/udp"
+                "80:80/tcp"
+                "443:443/tcp"
+                "443:443/udp"
+                "3000:3000/tcp"
+                "853:853/tcp"
+              ];
             };
           };
         };
       };
     };
+  };
 }
