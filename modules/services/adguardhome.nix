b5a94ec7cdeb4590cac8208392a25496d48a294a
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2026-08-17 16:03:57 -0700

    default settings

--- modules/services/adguardhome.nix
+++ modules/services/adguardhome.nix
@@ -2,10 +2,7 @@
   flake.modules.nixos.adguardhome = {
     services.adguardhome = {
       enable = true;
-      host = "127.0.0.1";
-      port = 3000;
       openFirewall = true;
-      settings = { };
     };
   };
 }
