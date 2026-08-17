e1e1cb1f0688997912b75fa318026f409be01cba
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2026-08-17 13:28:56 -0700

    updates

--- modules/services/adguardhome.nix
+++ modules/services/adguardhome.nix
@@ -3,19 +3,14 @@
     networking.firewall = {
       allowedTCPPorts = [
         53
-        5300
         443
-        4430
         3000
         8000
         853
-        8530
       ];
       allowedUDPPorts = [
         53
-        5300
         443
-        4430
       ];
     };
   };
