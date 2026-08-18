d0928d936989d05d748df84eee2b38b292f211b1
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2026-08-17 23:43:13 -0700

    removed extra ports

--- modules/services/adguardhome.nix
+++ modules/services/adguardhome.nix
@@ -2,18 +2,13 @@
   flake.modules.nixos.adguardhome = {
     services.adguardhome = {
       enable = true;
-      port = 8080;
     };
     networking.firewall = {
       allowedTCPPorts = [
         53
-        443
-        3000
-        8080
       ];
       allowedUDPPorts = [
         53
-        443
       ];
     };
   };
