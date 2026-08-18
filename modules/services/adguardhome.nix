06f40ece166419ca2b135f880833800fb103e7eb
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2026-08-17 20:56:01 -0700

    removed host and port

--- modules/services/adguardhome.nix
+++ modules/services/adguardhome.nix
@@ -2,8 +2,6 @@
   flake.modules.nixos.adguardhome = {
     services.adguardhome = {
       enable = true;
-      host = "0.0.0.0";
-      port = 3000;
     };
     networking.firewall = {
       allowedTCPPorts = [
