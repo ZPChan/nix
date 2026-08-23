bcd5b378984e4318138019f44777ecf17a12328b
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2026-08-23 09:11:29 -0700

    added wifi network

--- modules/services/wifi.nix
+++ modules/services/wifi.nix
@@ -3,12 +3,8 @@
     networking.wireless = {
       enable = true;
       secretsFile = "/run/secrets/wifi";
-      networks = {
-        main = {
-          ssid = "Dorkfest";
-          pskRaw = "ext:main_psk";
-        };
-      };
+      networks.Dorkfest.pskRaw = "ext:main_psk";
+      networks.ATT6XER3NI.pskRaw = "ext:timberlane_psk";
     };
     sops.secrets.wifi.owner = "wpa_supplicant";
   };
