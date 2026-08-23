ed0bfe0fe73896055dc985975a42d0e153fd0d17
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2026-08-23 13:46:01 -0700

    removed dns entry

--- modules/services/wifi.nix
+++ modules/services/wifi.nix
@@ -1,11 +1,11 @@
 {
-  flake.modules.nixos.wifi = {
+  flake.modules.nixos.wifi = { config, ... }: {
+    sops.secrets.wifi.owner = "wpa_supplicant";
     networking.wireless = {
       enable = true;
-      secretsFile = "/run/secrets/wifi";
+      secretsFile = config.sops.secrets.wifi.path;
       networks.Dorkfest.pskRaw = "ext:main_psk";
       networks.ATT6XER3NI.pskRaw = "ext:timberlane_psk";
     };
-    sops.secrets.wifi.owner = "wpa_supplicant";
   };
 }
