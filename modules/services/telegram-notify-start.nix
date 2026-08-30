b411f9d888997581f775abf0326c170282833255
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2026-08-30 16:48:00 -0700

    modified script

--- modules/services/telegram-notify-start.nix
+++ modules/services/telegram-notify-start.nix
@@ -17,8 +17,8 @@
           User = "root";
           ExecStart = "${pkgs.writeShellScript "telegram-notify-start.sh" ''
 
-            apiKey=$(${config.sops.secrets."telegram/apiKey".path})
-            chatId=$(${config.sops.secrets."telegram/chatId".path})
+            apiKey=$(cat ${config.sops.secrets."telegram/apiKey".path})
+            chatId=$(cat ${config.sops.secrets."telegram/chatId".path})
 
             curl -X POST "https://api.telegram.org/bot''$apiKey/sendMessage" -d "chat_id=''$chatId&text=Hello"
 
