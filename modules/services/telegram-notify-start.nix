4ec1825a8ecfe4bebecd5ece1bd912a1a2480353
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2026-08-30 17:36:35 -0700

    added some echos to the script

--- modules/services/telegram-notify-start.nix
+++ modules/services/telegram-notify-start.nix
@@ -13,8 +13,11 @@
         wants = [ "network-online.target" ];
         wantedBy = [ "multi-user.target" ];
         script = ''
+          echo "Accessing secrets..."
           apiKey=$(cat ${config.sops.secrets."telegram/apiKey".path})
           chatId=$(cat ${config.sops.secrets."telegram/chatId".path})
+
+          echo $(which curl)
 
           curl -X POST "https://api.telegram.org/bot''$apiKey/sendMessage" -d "chat_id=''$chatId&text=Hello"
         '';
