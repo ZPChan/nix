e834041b20f7edb7bf1aa576acf0a89eb5a70472
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2026-08-31 09:47:57 -0700

    removed secrets from desktop

--- modules/hosts/desktop/configuration.nix
+++ modules/hosts/desktop/configuration.nix
@@ -29,8 +29,6 @@ in
       zach-auto-login
     ];
 
-    sops.secrets.wifi = { };
-
     home-manager.users.zach = {
       imports = with inputs.self.modules.homeManager; [
         cli-tools-advanced
